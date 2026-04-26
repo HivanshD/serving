3b23902f334c4aef61190a92999672d416ce74c5
Karuna Venkatesh <fk2496@nyu.edu>
2026-04-26 14:03:26 +0000

    Add GISMo training pipeline: model, inference, Docker, config, cron

--- retrain_cron.sh
+++ retrain_cron.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
-LOG=/home/cc/retrain_cron.log
-echo "[$(date)] Retraining triggered..." >> $LOG
+# ForkWise auto-retrain every 6 hours
+LOG=/tmp/retrain_$(date +%Y%m%d_%H%M%S).log
+echo "Starting retrain at $(date)" >> $LOG
 
-docker run --rm   -v /home/cc/ml-sys-ops-project:/workspace   --gpus all --shm-size=12g --network host   -e MLFLOW_TRACKING_URI=http://129.114.108.56:5000   -e OS_ENDPOINT=https://chi.tacc.chameleoncloud.org:7480   -e OS_ACCESS_KEY=8921c48faf83433db2b1439a9b2889fd   -e OS_SECRET_KEY=7d1ce78efc5a48019888c9f3fa8ba2dd   train:latest python training/train.py     --config training/config.yaml     --dataset /workspace/data/processed/train.json     --embed_dim 4096 --lr 0.0001 --epochs 100     --batch_size 512 --margin 1.2     --run_name auto-retrain-$(date +%Y%m%d-%H%M%S) >> $LOG 2>&1
+/usr/bin/python3 /home/cc/train_gismo_gnn.py >> $LOG 2>&1
 
-echo "[$(date)] Training done. Backing up MLflow DB..." >> $LOG
+echo "Retrain done at $(date)" >> $LOG
 
-# Backup MLflow DB immediately after training
-docker cp forkwise-mlflow:/mlflow/db/mlflow.db /home/cc/mlflow_backup.db
-
+# Backup MLflow DB to S3
 python3 -c "
-import boto3
+import boto3, subprocess, datetime
 s3 = boto3.client('s3',
     endpoint_url='https://chi.tacc.chameleoncloud.org:7480',
     aws_access_key_id='8921c48faf83433db2b1439a9b2889fd',
     aws_secret_access_key='7d1ce78efc5a48019888c9f3fa8ba2dd',
     region_name='us-east-1')
-s3.upload_file('/home/cc/mlflow_backup.db', 'data-proj01', 'mlflow-backup/mlflow.db')
-print('MLflow DB backed up')
+subprocess.run(['docker', 'cp', 'forkwise-mlflow:/mlflow/db/mlflow.db', '/tmp/mlflow_backup.db'])
+ts = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+with open('/tmp/mlflow_backup.db', 'rb') as f:
+    s3.put_object(Bucket='data-proj01', Key='mlflow-backup/mlflow_' + ts + '.db', Body=f)
+print('MLflow backup done')
 " >> $LOG 2>&1
 
-echo "[$(date)] All done. Artifacts + MLflow DB backed up to data-proj01." >> $LOG
+echo "Backup done at $(date)" >> $LOG
