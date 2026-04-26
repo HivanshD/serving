51fece63be3f2ee1697a54f745bfb3d6342940c1
karunaav <vkaruna2002@gmail.com>
2026-04-26 00:53:57 +0000

    Update retrain cron: auto backup MLflow DB after every training run

--- retrain_cron.sh
+++ retrain_cron.sh
@@ -4,6 +4,9 @@ echo "[$(date)] Retraining triggered..." >> $LOG
 
 docker run --rm   -v /home/cc/ml-sys-ops-project:/workspace   --gpus all --shm-size=12g --network host   -e MLFLOW_TRACKING_URI=http://129.114.108.56:5000   -e OS_ENDPOINT=https://chi.tacc.chameleoncloud.org:7480   -e OS_ACCESS_KEY=8921c48faf83433db2b1439a9b2889fd   -e OS_SECRET_KEY=7d1ce78efc5a48019888c9f3fa8ba2dd   train:latest python training/train.py     --config training/config.yaml     --dataset /workspace/data/processed/train.json     --embed_dim 4096 --lr 0.0001 --epochs 100     --batch_size 512 --margin 1.2     --run_name auto-retrain-$(date +%Y%m%d-%H%M%S) >> $LOG 2>&1
 
+echo "[$(date)] Training done. Backing up MLflow DB..." >> $LOG
+
+# Backup MLflow DB immediately after training
 docker cp forkwise-mlflow:/mlflow/db/mlflow.db /home/cc/mlflow_backup.db
 
 python3 -c "
@@ -17,4 +20,4 @@ s3.upload_file('/home/cc/mlflow_backup.db', 'data-proj01', 'mlflow-backup/mlflow
 print('MLflow DB backed up')
 " >> $LOG 2>&1
 
-echo "[$(date)] Done." >> $LOG
+echo "[$(date)] All done. Artifacts + MLflow DB backed up to data-proj01." >> $LOG
